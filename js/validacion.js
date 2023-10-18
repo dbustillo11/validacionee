6a721b93d4d55a12c06506f6734a4c98d53480e5
dbustillo11 <bustillo.contacto@gmail.com>
2023-10-18 19:34:36 -0300

    SEEEEE

--- js/validacion.js
+++ js/validacion.js
@@ -1,40 +1,67 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const formulario = document.querySelector("form");
-    
-    formulario.addEventListener("submit", function(event) {
-        const nombre = document.getElementById("nombre").value.trim();
-        const apellido = document.getElementById("apellido").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const password1 = document.getElementById("password1").value;
-        const password2 = document.getElementById("password2").value;
-        const terminos = document.getElementById("terminos").checked;
-
-        // Validar si el nombre o apellido está vacío
-        if (!nombre || !apellido) {
-            alert("El nombre y el apellido son obligatorios.");
-            event.preventDefault();
-            return;
-        }
-
-        // Validar formato de email
-        const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-        if (!regexEmail.test(email)) {
-            alert("Por favor, introduce un email válido.");
-            event.preventDefault();
-            return;
-        }
-
-        // Validar si las contraseñas coinciden
-        if (password1 !== password2) {
-            alert("Las contraseñas no coinciden.");
-            event.preventDefault();
-            return;
-        }
-
-        // Validar si se han aceptado los términos y condiciones
-        if (!terminos) {
-            alert("Debes aceptar los términos y condiciones del servicio.");
-            event.preventDefault();
-        }
-    });
+    const form = document.querySelector('form');
+    form.addEventListener('submit', validarFormulario);
 });
+
+function validarFormulario(event) {
+    event.preventDefault();
+    
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terminosModal = document.getElementById("terminosModal").checked;
+
+    if (nombre === "") {
+        mostrarError("nombre", "Debe ingresar un nombre.");
+    } else {
+        ocultarError("nombre");
+    }
+
+    if (apellido === "") {
+        mostrarError("apellido", "Debe ingresar un apellido.");
+    } else {
+        ocultarError("apellido");
+    }
+
+    if (email === "" || !email.includes('@')) {
+        mostrarError("email", "Debe ingresar un email válido.");
+    } else {
+        ocultarError("email");
+    }
+
+    if (password1 === "") {
+        mostrarError("password1", "Debe ingresar una contraseña.");
+    } else {
+        ocultarError("password1");
+    }
+
+    if (password2 === "" || password1 !== password2) {
+        mostrarError("password2", "Las contraseñas deben coincidir.");
+    } else {
+        ocultarError("password2");
+    }
+
+    if (!terminosModal) {
+        mostrarError("terminosModal", "Debe aceptar los términos y condiciones.");
+    } else {
+        ocultarError("terminosModal");
+    }
+}
+
+function mostrarError(campo, mensaje) {
+    let input = document.getElementById(campo);
+    let errorDiv = document.getElementById(campo + "Error");
+
+    input.classList.add("is-invalid");
+    errorDiv.textContent = mensaje;
+}
+
+function ocultarError(campo) {
+    let input = document.getElementById(campo);
+    let errorDiv = document.getElementById(campo + "Error");
+
+    input.classList.remove("is-invalid");
+    errorDiv.textContent = "";
+}
