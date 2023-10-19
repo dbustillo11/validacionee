24099942648ab1f326e2f4e4644615d393a1cbef
tahirivazquez <tahiwdev@gmail.com>
2023-10-18 21:37:47 -0300

    validacion en tiempo real

--- js/validacion.js
+++ js/validacion.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector('form');
     form.addEventListener('submit', validarFormulario);
 });
 
 function validarFormulario(event) {
     event.preventDefault();
-    
+
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let email = document.getElementById("email").value;
@@ -48,6 +48,8 @@ function validarFormulario(event) {
     } else {
         ocultarError("terminosModal");
     }
+
+    validacionTiempoReal();
 }
 
 function mostrarError(campo, mensaje) {
@@ -56,6 +58,7 @@ function mostrarError(campo, mensaje) {
 
     input.classList.add("is-invalid");
     errorDiv.textContent = mensaje;
+    errorDiv.style.display = "block";
 }
 
 function ocultarError(campo) {
@@ -64,4 +67,71 @@ function ocultarError(campo) {
 
     input.classList.remove("is-invalid");
     errorDiv.textContent = "";
+    errorDiv.style.display = "none";
 }
+
+function validacionTiempoReal() {
+
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let password1 = document.getElementById("password1");
+    let password2 = document.getElementById("password2");
+    let terminosModal = document.getElementById("terminosModal");
+
+    nombre.addEventListener('input', (evento) => {
+        const valorInput = evento.target.value;
+        if (valorInput === "") {
+            mostrarError("nombre", "Debe ingresar un nombre.");
+        } else {
+            ocultarError("nombre");
+        }
+    })
+
+    apellido.addEventListener('input', (evento) => {
+        const valorInput = evento.target.value;
+        if (valorInput === "") {
+            mostrarError("apellido", "Debe ingresar un apellido.");
+        } else {
+            ocultarError("apellido");
+        }
+    })
+
+    email.addEventListener('input', (evento) => {
+        const valorInput = evento.target.value;
+        if (valorInput === "" || !valorInput.includes('@')) {
+            mostrarError("email", "Debe ingresar un email válido.");
+        } else {
+            ocultarError("email");
+        }
+    })
+
+    password1.addEventListener('input', (evento) => {
+        const valorInput = evento.target.value;
+        if (valorInput === "") {
+            mostrarError("password1", "Debe ingresar una contraseña.");
+        } else {
+            ocultarError("password1");
+        }
+    })
+
+    password2.addEventListener('input', (evento) => {
+        const valorInput = evento.target.value;
+        if (valorInput === "" || password1.value !== valorInput) {
+            mostrarError("password2", "Las contraseñas deben coincidir.");
+        } else {
+            ocultarError("password2");
+        }
+    })
+
+    terminosModal.addEventListener('change', (evento) => {
+        const valorInput = evento.target.value;
+        if (!valorInput) {
+            mostrarError("terminosModal", "Debe aceptar los términos y condiciones.");
+        } else {
+            ocultarError("terminosModal");
+        }
+    })
+
+}
+
